1c1f3d75c6a013bed59e74a34a28263dcbabcc17
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2023-09-18 16:22:27 +0100

    hmmm

--- asset/javascript/index.js
+++ asset/javascript/index.js
@@ -3,20 +3,22 @@ const navToggle = document.querySelector(".nav-bar-toggle-open");
 const bannerSection = document.querySelector("#banner");
 const heroSection = document.querySelector(".hero");
 
+
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     if (navMenu.classList.contains("active")) {
-        bannerSection.style.transform = "translateY(350px)"; // Adjust this value based on your navigation menu height
+        bannerSection.style.transform = "translateY(350px)";
     } else {
         bannerSection.style.transform = "translateY(0)";
     }
-}); 
+});
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
-    const scrollSpeed = 0.5; // Adjust the scroll speed factor
-    
+    const scrollSpeed = 0.5;
     heroSection.style.backgroundPositionY = `calc(50% + ${scrollY * scrollSpeed}px)`;
+    const nav = document.getElementById("nav-el");
+    nav.classList.toggle("nav-white", window.scrollY > 0);
 });
 
 
